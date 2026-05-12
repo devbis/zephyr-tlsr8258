f692584864c8ddc039c414cf31f3af51f3a81f55
Ivan Belokobylskiy <belokobylskij@gmail.com>
2026-05-12 12:22:14 +0400

    zigbee: fix task queue ring full handling

--- tests/subsys/zigbee/task_post_queue/src/main.c
+++ tests/subsys/zigbee/task_post_queue/src/main.c
@@ -21,4 +21,22 @@ ZTEST(task_post_queue, test_task_post_executes_callback)
 	zassert_equal(called, 3);
 }
 
+ZTEST(task_post_queue, test_task_post_queue_full_and_wrap)
+{
+	const int queue_slots = 32;
+
+	called = 0;
+	for (int i = 1; i <= queue_slots; i++) {
+		zassert_equal(tl_zbTaskPost(test_cb, INT_TO_POINTER(i)), RET_OK);
+	}
+
+	zassert_equal(tl_zbTaskPost(test_cb, INT_TO_POINTER(99)), RET_BUSY);
+	zb_taskq_run_pending_for_test();
+	zassert_equal(called, ((queue_slots * (queue_slots + 1)) / 2));
+
+	zassert_equal(tl_zbTaskPost(test_cb, INT_TO_POINTER(5)), RET_OK);
+	zb_taskq_run_pending_for_test();
+	zassert_equal(called, ((queue_slots * (queue_slots + 1)) / 2) + 5);
+}
+
 ZTEST_SUITE(task_post_queue, NULL, NULL, NULL, NULL, NULL);
