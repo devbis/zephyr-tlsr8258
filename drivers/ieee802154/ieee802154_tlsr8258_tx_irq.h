a59aea6613704ede73a0f64e399f387bc038cd29
Ivan Belokobylskiy <belokobylskij@gmail.com>
2026-05-26 16:36:37 +0400

    ieee802154: harden tlsr8258 rx handoff

---
+++ drivers/ieee802154/ieee802154_tlsr8258_tx_irq.h
@@ -0,0 +1,9 @@
+#ifndef IEEE802154_TLSR8258_TX_IRQ_H_
+#define IEEE802154_TLSR8258_TX_IRQ_H_
+
+#include <stdbool.h>
+#include <stdint.h>
+
+bool tlsr8258_tx_irq_indicates_success(uint16_t irq);
+
+#endif /* IEEE802154_TLSR8258_TX_IRQ_H_ */
